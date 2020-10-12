dbc5006ffec5730494d2de82efc69a2c02d65669
oucifkarim01 <oucifkarim01@gmail.com>
2020-10-12 19:57:59 +0100

    fst

---
+++ js/script.js
@@ -0,0 +1,15 @@
+initAboutUsImageSlider();
+
+
+function initAboutUsImageSlider(){
+    let active_image = document.getElementById("active__image");
+    let image_selecter = document.getElementById("image__selecter");
+
+    image_selecter.addEventListener("click",function(ev){
+        if(ev.target.className === "image_link"){
+
+        }
+    })
+
+
+}
