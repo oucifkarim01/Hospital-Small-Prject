eed11d6c868f8308223360f040472a2b59d346c5
oucifkarim01 <oucifkarim01@gmail.com>
2020-10-12 20:14:05 +0100

    fst

--- js/script.js
+++ js/script.js
@@ -1,15 +1,20 @@
 initAboutUsImageSlider();
 
 
-function initAboutUsImageSlider(){
-    let active_image = document.getElementById("active__image");
-    let image_selecter = document.getElementById("image__selecter");
 
+function initAboutUsImageSlider(){
+    let active_image = document.querySelector("#active__image img");
+    let image_selecter = document.getElementById("image__selecter");
+    document.getElementsByTagName
     image_selecter.addEventListener("click",function(ev){
         if(ev.target.className === "image_link"){
-
+            let imgsrc = ev.target.getElementsByTagName("img")[0].src;
+            console.log(imgsrc);
+            active_image.parentElement.classList.add("trans");
+            setTimeout(()=>{
+                active_image.src = imgsrc;
+                active_image.parentElement.classList.remove("trans");      
+            },200)
         }
-    })
-
-
+    });
 }
